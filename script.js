906792ca7f80cb078d10322c85789ca36509c5b6
wei-nete <wei@nete.com>
2016-04-08 16:12:24 -0400

    #12 Create Button

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ var div1Click = document.querySelector('.div1');
 div1Click.addEventListener('click', function(){
   console.log("alert");
 });
+
+myfunction=function() {
+	alert("button pressed");
+}
